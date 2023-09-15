5f989548735d1c23ffeb465eb777bfc5274dbacb
nazifbara <nazifbarassounon@gmail.com>
2023-09-15 17:12:05 +0100

    add initial styling

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+	import 'open-props/style'
+	import 'open-props/normalize'
+
+	import '../app.css'
+</script>
+
+<div>
+	<main>
+		<slot />
+	</main>
+</div>
+
+<style>
+	div {
+		height: 100%;
+	}
+</style>
