10f9aad129ede8929621f183379112aef3f118a7
nazifbara <nazifbarassounon@gmail.com>
2023-09-15 15:28:16 +0100

    first commit

---
+++ svelte.config.js
@@ -0,0 +1,13 @@
+import { preprocessMeltUI } from '@melt-ui/pp';
+import sequence from 'svelte-sequential-preprocessor';
+import adapter from '@sveltejs/adapter-auto';
+import { vitePreprocess } from '@sveltejs/kit/vite';
+
+/** @type {import('@sveltejs/kit').Config}*/
+const config = {
+	preprocess: sequence([vitePreprocess(), preprocessMeltUI()]),
+	kit: {
+		adapter: adapter()
+	}
+};
+export default config;
