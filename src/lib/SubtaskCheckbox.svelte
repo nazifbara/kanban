ce2cbfdecbefd30cf6c25da4b032b8f6ec2de7f8
nazifbara <nazifbarassounon@gmail.com>
2023-09-18 16:12:52 +0100

    create the subtask checkbox

---
+++ src/lib/SubtaskCheckbox.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+	export let name = ''
+	export let checked = false
+</script>
+
+<label class="surface-1 body-m">
+	<input type="checkbox" {name} {checked} />
+	<span><slot /></span>
+</label>
+
+<style lang="postcss">
+	label {
+		color: var(--text-1);
+		padding: var(--size-3);
+		border-radius: var(--radius-2);
+		display: flex;
+		align-items: center;
+		gap: var(--size-3);
+		transition: background-color ease-in 150ms;
+
+		&:hover {
+			background-color: hsl(var(--primary-1-hsl) / 25%);
+		}
+	}
+
+	input:checked ~ span {
+		text-decoration: line-through;
+		color: hsl(var(--text-1-hsl) / 50%);
+	}
+</style>
---
+++ src/routes/components/input/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { SubtaskCheckbox } from '$lib'
+</script>
+
+<div class="surface-2">
+	<SubtaskCheckbox>Lorem ipsum dolor sit amet</SubtaskCheckbox>
+</div>
+
+<style>
+	div {
+		display: grid;
+		gap: 20px;
+		justify-items: center;
+		padding-block: 30px;
+	}
+</style>
