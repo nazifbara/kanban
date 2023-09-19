a33462875196cff01c5f76ad737c1c8d6b6ef124
nazifbara <nazifbarassounon@gmail.com>
2023-09-19 17:37:35 +0100

    set the boards' store in the context

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,8 +1,17 @@
 <script lang="ts">
 	import 'open-props/style'
 	import 'open-props/normalize'
+	import { setContext } from 'svelte'
 
 	import '../app.css'
+	import data from '$lib/data.json'
+	import { createBoards } from '$lib/boards'
+
+	const boards = createBoards(data.boards)
+
+	setContext('store', {
+		boards
+	})
 </script>
 
 <div>
