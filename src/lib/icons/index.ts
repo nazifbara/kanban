a47e4bed21d56b139788d19c01aff4ccc57cc6a3
nazifbara <nazifbarassounon@gmail.com>
2023-09-24 19:31:45 +0100

    show boards menu

--- src/lib/icons/index.ts
+++ src/lib/icons/index.ts
@@ -1,9 +1,15 @@
+import DarkTheme from './dark-theme.svelte'
+import LightTheme from './light-theme.svelte'
+import Board from './board.svelte'
 import ChevronDown from './chevron-down.svelte'
 import LogoMobile from './logo-mobile.svelte'
 import VerticalEllipsis from './vertical-ellipsis.svelte'
 import AddTaskMobile from './add-task-mobile.svelte'
 
 export const iconsMap = {
+	DarkTheme,
+	LightTheme,
+	Board,
 	ChevronDown,
 	LogoMobile,
 	VerticalEllipsis,
