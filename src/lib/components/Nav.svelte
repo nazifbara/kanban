42201e70e8b46812e2c4d937594632f602a37459
nazifbara <nazifbarassounon@gmail.com>
2023-11-22 18:56:33 +0100

    refactor navigation styling

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -17,13 +17,13 @@
 
 <BoardForm isOpen={isAddingBoard} type="create" />
 
-<nav>
-	<h3 class="text text-body-m">ALL BOARDS ({$boards.items.length})</h3>
+<nav class="nav">
+	<h3 class="nav__heading text text-body-m">ALL BOARDS ({$boards.items.length})</h3>
 	{#each $boards.items as board, i}
 		<button
 			on:click={() => onBoardChange(i)}
-			class="nav-item"
-			class:active={$currentBoard && board.name === $currentBoard.name}
+			class="nav__item"
+			class:nav__item--active={$currentBoard && board.name === $currentBoard.name}
 		>
 			<span>
 				<Icon name="Board" />
@@ -31,37 +31,88 @@
 			</span>
 		</button>
 	{/each}
-	<button class="nav-item" on:click={() => ($isAddingBoard = true)}
+	<button class="nav__item" on:click={() => ($isAddingBoard = true)}
 		><span><Icon name="Board" /> + Create New Board</span></button
 	>
 </nav>
 
 <style lang="postcss">
-	nav > * {
+	.nav {
+		display: grid;
+		margin-bottom: var(--size-3);
+	}
+
+	.nav__heading {
+		margin-block-end: var(--size-3);
+		color: var(--text-2);
+		letter-spacing: 0.3em;
+		margin-bottom: var(--size-6);
 		padding-inline-start: var(--size-5);
 	}
 
-	nav {
-		display: grid;
-		margin-bottom: var(--size-3);
+	.nav__item {
+		--_bg-clr: var(--bg-clr, transparent);
+		--_bg-hover: var(--bg-hover, transparent);
+		--_text-clr: var(--text-clr, inherit);
+		--_text-clr-hover: var(--text-clr-hover, var(--primary-1));
 
-		& h3 {
-			margin-block-end: var(--size-3);
-			color: var(--text-2);
-			letter-spacing: 0.3em;
-			margin-bottom: var(--size-6);
+		display: flex;
+		align-items: center;
+		height: 4rem;
+		border-end-end-radius: var(--radius-round);
+		border-start-end-radius: var(--radius-round);
+		padding-inline-start: var(--size-5);
+		padding-inline-end: var(--size-3);
+		font-size: var(--font-size-3);
+		background-color: var(--_bg-clr);
+		color: var(--_text-clr);
+		transition: background-color 300ms var(--ease-5);
+
+		&:hover {
+			--_bg-clr: var(--_bg-hover);
+			--_text-clr: var(--_text-clr-hover);
+
+			:global(svg path) {
+				fill: var(--primary-1);
+			}
 		}
 
-		& .nav-item:last-child {
-			color: var(--primary-1);
+		:global([color-scheme='dark']) &:is(:not(.nav__item--active)):not(:last-child) {
+			--bg-hover: white;
+		}
 
-			&:hover {
-				background-color: transparent;
-			}
+		:global([color-scheme='light']) &:is(:not(.nav__item--active)):not(:last-child) {
+			--bg-hover: hsl(var(--primary-1-hsl) / 10%);
+		}
+
+		& > span {
+			display: flex;
+			align-items: center;
+			gap: var(--size-2);
+		}
+
+		&:last-child {
+			--text-clr: var(--primary-1);
+			--bg-hover: transparent;
 
 			& :global(svg path) {
 				fill: var(--primary-1);
 			}
+		}
+	}
+
+	.nav__item--active {
+		--text-clr: white;
+		--text-clr-hover: var(--text-clr);
+		--bg-clr: var(--primary-1);
+		--bg-hover: var(--bg-clr);
+
+		:global(svg path) {
+			fill: white;
+		}
+
+		&:hover :global(svg path) {
+			fill: white;
 		}
 	}
 </style>
